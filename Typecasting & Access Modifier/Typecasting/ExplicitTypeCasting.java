b5423cf070e0bad5bf5c5fe989309f160181003a
unknown <atharvaborale202000@gmail.com>
2022-01-30 17:56:14 +0530

    AccessSpecifier and TypeCasting

---
+++ Typecasting & Access Modifier/Typecasting/ExplicitTypeCasting.java
@@ -0,0 +1,14 @@
+package typeCasting;
+
+public class ExplicitTypeCasting {
+
+
+		public static void main(String[] args) {
+			
+			double a=12.56;
+			
+			int b=(int) a;
+			
+			System.out.println("Converted double "+a+ " to int "+b);
+		}
+}
